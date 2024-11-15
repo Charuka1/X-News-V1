ab4386e53361191f50facdaf6dad997eb003d0be
Charuka1 <129839125+Charuka1@users.noreply.github.com>
2024-11-15 09:40:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -75,9 +75,9 @@ ${data.desc}
 *\`Url\`*
 ──────────────────────`
 
-         let newss = await news1.findOne({ id: '12345' }) 
+         let newss = await news1.findOne({ id: '12345678' }) 
   if (!newss) { 
-         await new news1({ id: '12345', newsid: data.id, events:'true' }).save() 
+         await new news1({ id: '12345678', newsid: data.id, events:'true' }).save() 
            await session.sendMessage("120363320172873548@g.us",{image:{url: data.image}, caption:mg},{ ephemeralExpiration: WA_DEFAULT_EPHEMERAL })
      } else { 
          if(newss.newsid == data.id )  
@@ -85,7 +85,7 @@ ${data.desc}
            return 
           } 
           else{ 
-             await news1.updateOne({ id: '12345' }, { newsid : data.id, events:'true'}) 
+             await news1.updateOne({ id: '12345678' }, { newsid : data.id, events:'true'}) 
              await session.sendMessage("120363320172873548@g.us",{image:{url: data.image}, caption:mg},{ ephemeralExpiration: WA_DEFAULT_EPHEMERAL })
           }
      } 
